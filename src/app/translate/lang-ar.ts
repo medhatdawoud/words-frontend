09bf3564ac901b3c9ea163e610aebea146e6a716
Medhat Dawoud <medhatdawoud@gmail.com>
2018-04-04 00:14:32 +0200

    Added the missed translations for arabic language

--- src/app/translate/lang-ar.ts
+++ src/app/translate/lang-ar.ts
@@ -13,5 +13,18 @@ export const LANG_AR_TRANS = {
     'add_word.form.tags': 'علامات',
     'add_word.form.expand_form.message': 'اضفط لاضافة تفاصيل اكتر ..',
     'add_word.form.delete_button': 'حذف',
-    'add_word.form.save_button': 'حفظ الكلمة'
+    'add_word.form.save_button': 'حفظ الكلمة',
+
+    'add_word.form.remove_button': 'حذف',
+    'add_word.form.add_button': 'اضافة',
+    'add_word.form.add_new_word_button': 'اضافة كلمة جديدة',
+
+    'filter_word.form.search_by_word_input': 'بحث بالكلمة او العلامات او المترادفات',
+    'filter_word.form.sort_by_newest': 'الاحدث اولا',
+    'filter_word.form.sort_by_oldest': 'الاقدم اولا',
+    'filter_word.form.sort_by_word_az': 'بالكلمات تصاعديا',
+    'filter_word.form.sort_by_word_za': 'بالكلمات تنازليا',
+
+    'view_word.modal.heading' : 'تفاضيل الكلمة',
+    'view_word.modal.close_button': 'اغلاق',
 };
